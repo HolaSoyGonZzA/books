3692301051d1e6b5c6c7b545e17ceb2dabc36db4
Roberth González <roberth.gonzalez@adalid.net>
2022-09-26 21:19:37 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const BOOKS = [];
+
+class Book {
+  constructor(name, author, year) {
+    this.name = name;
+    this.author = author;
+    this.year = year;
+  }
+  info() {
+    return `${this.name} - ${this.author} ${this.year}`;
+  }
+  updateYear(year) {
+    this.year = year;
+  }
+}
+
+console.log(BOOKS);
+
+const b1 = new Book("La Novia Gitana", "Carmen Mola", 2015);
+
+BOOKS.push(b1);
+
+const b2 = new Book("La Red Púrpura", "Carmen Mola", 2019);
+
+BOOKS.push(b2);
+
+console.log(BOOKS);
+
+BOOKS.shift();
+
+console.log(BOOKS);
+
+BOOKS.unshift(new Book("jajax", "ajax", 2022));
+
+console.log(BOOKS);
